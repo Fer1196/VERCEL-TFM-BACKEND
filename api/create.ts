23d8b2cee9ae62e18cffde2ab1cbb8839c0579f4
Fer1196 <rodifer1196@hotmail.com>
2024-08-11 17:12:30 -0500

    Update create.ts

--- api/create.ts
+++ api/create.ts
@@ -14,6 +14,6 @@ export default (req: VercelRequest, res: VercelResponse) => {
         data,
       });
   } else {
-    res.status(405).json({ message: "Método no permitid revisaro" });
+    res.status(405).json({ message: "Método no permitio revisarlo ahora mismo" });
   }
 };
